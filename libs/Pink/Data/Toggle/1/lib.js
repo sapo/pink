00b3da452ec3c4d903f8ffd52f1562f085d59e21
Hugo Lima <hugo-m-lima@telecom.pt>
2015-02-06 16:01:40 +0000

    Toggle handler bug fix

--- libs/Pink/Data/Toggle/1/lib.js
+++ libs/Pink/Data/Toggle/1/lib.js
@@ -5,29 +5,36 @@
  * @version 1
  */
 
-Ink.createModule('Pink.Data.Toggle', '1', ['Pink.Data.Binding_1', 'Ink.UI.Toggle_1'], function(ko, Toggle) {
+Ink.createModule('Pink.Data.Toggle', '1', ['Pink.Data.Binding_1', 'Ink.UI.Toggle_1', 'Ink.Dom.Event_1'], function(ko, Toggle, InkEvent) {
     /*
      * Toggle binding handler
      * 
      */
     ko.bindingHandlers.toggle = {
-        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
             var attr;
             var binding = ko.unwrap(valueAccessor());
-            var options = {}; // sensible defaults 
+            var options = {}; // sensible defaults
+            var toggle;
 
+            // If the element is already a toggle trigger then exit 
+            if (element.hasAttribute('data-is-toggle-trigger')) {
+                return;
+            }
+            
             if (typeof binding == 'object') {
                 for (attr in binding) {
                     options[attr] = ko.unwrap(binding[attr]);
                 }
             }
 
-            // Defer the toggle renderization to wait for the target element to be in the DOM
-            window.setTimeout(function() {
-                if (document.getElementById(options.target.slice(1))) {
-                    new Toggle(element, options);
-                }
-            }, 0);
+            if (document.getElementById(options.target.slice(1))) {
+                toggle = new Toggle(element, options);
+
+                ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                    InkEvent.stopObserving(element);
+                });
+            }
         }
     };
 
