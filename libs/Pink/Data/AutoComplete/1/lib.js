0dd95d888f5e7866bc183992769808d66bf6696f
Eduardo Cunha <eduardo-j-cunha@telecom.pt>
2015-05-14 11:52:55 +0100

    Autocomplete widget: Added new parameter to set focus on the input element

--- libs/Pink/Data/AutoComplete/1/lib.js
+++ libs/Pink/Data/AutoComplete/1/lib.js
@@ -1,7 +1,7 @@
 /**
  * @module Pink.Data.AutoComplete
  * @desc AutoComplete widget
- * @author intra-team  AT sapo.pt
+ * @author intra-team AT sapo.pt
  * @version 1
  */
 
@@ -9,40 +9,40 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
     /*
      * This function must be bound to an options object
      */
-	function handleValueChange(item) {
+    function handleValueChange(item) {
         var labelToWrite = item ? item.label : undefined;
         var valueToWrite = item ? item.value : undefined;
 
         if (this.allowAny && valueToWrite == undefined) {
-        	labelToWrite = valueToWrite = this.displayInput.value;
+            labelToWrite = valueToWrite = this.displayInput.value;
         }
-        
+
         if (valueToWrite != undefined) {
             updateValueAndLabel(this.binding, labelToWrite, valueToWrite, this.displayInput, this.element);
         } else { //They did not make a valid selection so change the autoComplete box back to the previous selection
             var currentModelValue = ko.unwrap(this.binding.value);
-            
+
             //If the currentModelValue exists and is not nothing, then find out the display
             // otherwise just blank it out since it is an invalid value
             if (!currentModelValue) {
                 this.displayInput.value = '';
             } else {
                 //Go through the source and find the id, and use its label to set the autocomplete
-                var selectedItem = findSelectedItem(this.dataSource, this.binding, currentModelValue);           
+                var selectedItem = findSelectedItem(this.dataSource, this.binding, currentModelValue);
 
                 //If we found the item then update the display
                 if (selectedItem) {
                     var displayText = this.labelProp ? ko.unwrap(selectedItem[this.labelProp]) : ko.unwrap(selectedItem).toString();
                     this.displayInput.value = displayText;
                 } else { //if we did not find the item, then just blank it out, because it is an invalid value
-                	this.displayInput.value = '';
+                    this.displayInput.value = '';
                 }
             }
         }
 
         return false;
     };
-    
+
     function findSelectedItem(dataSource, binding, selectedValue) {
         var source = ko.unwrap(dataSource);
         var valueProp = ko.unwrap(binding.optionsValue);
@@ -55,23 +55,23 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
 
         return selectedItem;
     };
-    
+
     function buildDataSource(dataSource, labelProp, valueProp) {
         var source = ko.unwrap(dataSource);
         var mapped = ko.utils.arrayMap(source, function (item) {
             var result = {};
-            result.label = labelProp ? ko.unwrap(item[labelProp]) : ko.unwrap(item).toString();  //show in pop-up choices
-            result.value = valueProp ? ko.unwrap(item[valueProp]) : ko.unwrap(item).toString();  //value
+            result.label = labelProp ? ko.unwrap(item[labelProp]) : ko.unwrap(item).toString(); //show in pop-up choices
+            result.value = valueProp ? ko.unwrap(item[valueProp]) : ko.unwrap(item).toString(); //value
             result.source = item;
             return result;
         });
         return mapped;
     };
-    
+
     function updateValueAndLabel(binding, labelToWrite, valueToWrite, displayInput, element) {
         if (ko.isWriteableObservable(binding.value)) {
-        	binding.value(valueToWrite);
-        } else {  //write to non-observable
+            binding.value(valueToWrite);
+        } else { //write to non-observable
             if (binding['_ko_property_writers'] && binding['_ko_property_writers']['value']) {
                 binding['_ko_property_writers']['value'](valueToWrite);
                 //Because this is not an observable, we have to manually change the controls values
@@ -81,7 +81,7 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
             }
         }
     };
-    
+
     // Function to build the menu with the suggested options
     function buildOptions(ul, source, filter, itemTemplate, bindingContext, minFilterLen) {
         itemTemplate = itemTemplate || 'Pink.Data.AutoComplete.ItemTemplate';
@@ -99,51 +99,51 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
                     filter = inkStr.removeAccentedChars(filter);
                     filter = ".*" + filter.replace(/\s+/g, ".*") + ".*";
                 }
-                
+
                 tmpUl = document.createElement('ul');
-                
+
                 for (index=0; (index<source.length) && (items < ko.bindingHandlers.autoComplete.maxVisibleOptions); index++) {
                     label = inkStr.removeAccentedChars(source[index].label);
                     value = source[index].value;
-                    
+
                     if (filter && !label.match(new RegExp(filter, "i"))) {
                         continue;
                     }
-                    
+
                     li = document.createElement('li');
                     ko.renderTemplate(itemTemplate, new ko.bindingContext(source[index], bindingContext), {}, li);
                     ko.cleanNode(li);
                     tmpUl.appendChild(li);
                     items++;
                 }
-                
+
                 ul.innerHTML = tmpUl.innerHTML;
             }, 0);
         } else {
             ul.innerHTML = '';
         }
     };
-    
+
     /*
      * Function to transform the input into an autocomplete input
-     * 
+     *
      * @param options: object
-     * 
-     */ 
+     *
+     */
     function buildAutoComplete(options) {
         var nav = document.createElement('nav');
         var ul = document.createElement('ul');
         var displayOptions = options.displayOptions;
         var displayInput = options.displayInput;
         var activeItem;
-        
+
         nav.setAttribute('class', 'ink-navigation');
         ul.setAttribute('class', 'menu vertical');
         nav.appendChild(ul);
 
         buildOptions(ul, options.source, undefined, options.itemTemplate, options.bindingContext, options.minFilterLength);
         displayOptions.appendChild(nav);
-        
+
         // Handle input focus
         inkEvt.observe(displayInput, 'focus', function() {
             buildOptions(ul, options.source, displayInput.value, options.itemTemplate, options.bindingContext, options.minFilterLength);
@@ -170,12 +170,12 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
         inkEvt.observe(displayOptions, 'click', function(event) {
             activeItem = inkEvt.element(event);
             var inputValue = activeItem.getAttribute('data-label');
-            
+
             displayInput.value = inputValue;
             options.change({label: inputValue, value: activeItem.getAttribute('data-value')});
             buildOptions(ul, options.source, inputValue, options.itemTemplate, options.bindingContext, options.minFilterLength);
         }, true);
-        
+
         // Key entered in input control
         inkEvt.observe(displayInput, 'keyup', function(event) {
            var inputValue;
@@ -184,27 +184,27 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
 
            keyCode = event.keyCode;
            // Handle arrow keys and return
-           if ( (keyCode == inkEvt.KEY_DOWN) || 
-                (keyCode == inkEvt.KEY_UP) || 
-                (keyCode == inkEvt.KEY_RETURN) || 
-                (keyCode == inkEvt.KEY_LEFT) || 
+           if ( (keyCode == inkEvt.KEY_DOWN) ||
+                (keyCode == inkEvt.KEY_UP) ||
+                (keyCode == inkEvt.KEY_RETURN) ||
+                (keyCode == inkEvt.KEY_LEFT) ||
                 (keyCode == inkEvt.KEY_RIGHT) ) {
                inkEvt.stop(event);
-               
+
                if (keyCode == inkEvt.KEY_DOWN) {
                    if (!activeItem && ul.firstChild) {
                        activeItem = ul.firstChild;
                        activeItem.setAttribute('class', 'active');
                        return;
                    }
-                   
+
                    if (activeItem && inkEl.nextElementSibling(activeItem)) {
                        activeItem.setAttribute('class', '');
                        activeItem = inkEl.nextElementSibling(activeItem);
                        activeItem.setAttribute('class', 'active');
                    }
                }
-               
+
                if (keyCode == inkEvt.KEY_UP) {
                    if (activeItem && (activeItem == ul.firstChild)) {
                        activeItem.setAttribute('class', '');
@@ -212,14 +212,14 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
                        displayInput.select();
                        return;
                    }
-                   
+
                    if (activeItem && inkEl.previousElementSibling(activeItem)) {
                        activeItem.setAttribute('class', '');
                        activeItem = inkEl.previousElementSibling(activeItem);
                        activeItem.setAttribute('class', 'active');
                    }
                }
-               
+
                if (keyCode == inkEvt.KEY_RETURN) {
                    if (activeItem) {
                        element = Ink.s('a', activeItem);
@@ -230,50 +230,51 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
                        options.change({label: inputValue, value: element.getAttribute('data-value')});
                    }
                }
-               
+
                return false;
            }
-           
+
            activeItem = undefined;
            buildOptions(ul, options.source, displayInput.value, options.itemTemplate, options.bindingContext, options.minFilterLength);
            if (options.allowAny) {
-        	   options.change();
+               options.change();
            }
-        });                    
+        });
     };
-	
+
     /*
-     * Ink + Knockout autoComplete binding 
-     * 
+     * Ink + Knockout autoComplete binding
+     *
      */
     ko.bindingHandlers.autoComplete = {
         /*
          * Knockout custom binding init
-         * 
+         *
          */
         maxVisibleOptions: 20,
-            
+
         init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
             var opt = new (function() {
-            	this.dataSource = valueAccessor();
-            	this.binding = allBindingsAccessor();
-            	this.valueProp = ko.unwrap(this.binding.optionsValue);
-            	this.labelProp = ko.unwrap(this.binding.optionsText) || this.valueProp;
-            	this.allowAny = ko.unwrap(this.binding.allowAny);
-            	this.element = element;
-            	this.itemTemplate = ko.unwrap(this.binding.optionTemplate);
-            	this.bindingContext = bindingContext;
-            	this.minFilterLength = ko.unwrap(this.binding.minFilterLength);
-            	this.style = ko.unwrap(this.binding.pickerStyle) || 'autocomplete';
+                this.dataSource = valueAccessor();
+                this.binding = allBindingsAccessor();
+                this.valueProp = ko.unwrap(this.binding.optionsValue);
+                this.labelProp = ko.unwrap(this.binding.optionsText) || this.valueProp;
+                this.allowAny = ko.unwrap(this.binding.allowAny);
+                this.element = element;
+                this.itemTemplate = ko.unwrap(this.binding.optionTemplate);
+                this.bindingContext = bindingContext;
+                this.minFilterLength = ko.unwrap(this.binding.minFilterLength);
+                this.style = ko.unwrap(this.binding.pickerStyle) || 'autocomplete';
+                this.focus = ko.unwrap(this.binding.focus);
             })();
             var mappedSource;
             var subscription;
-            var placeholderText = opt.binding.attr && opt.binding.attr.placeholder ? opt.binding.attr.placeholder :  element.getAttribute('placeholder') || '';
+            var placeholderText = opt.binding.attr && opt.binding.attr.placeholder ? opt.binding.attr.placeholder : element.getAttribute('placeholder') || '';
             var childClass;
             var controlChild;
 
             element.style.display = 'none';
-            
+
             if (opt.style == 'autocomplete') {
                 childClass = 'append-button';
                 controlChild = '<span><input placeholder="' + placeholderText + '" type="text"></input></span><div class="ink-button"><i class="fa fa-times"></i></div>';
@@ -286,9 +287,9 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
 
             opt.displayElement = inkEl.htmlToFragment('<div class="pink-auto-complete control-group '+ element.getAttribute('class') +
                     '"><div class="control '+childClass+'">'+controlChild+'</div><div class="pink-auto-complete-options"></div></div>').firstChild;
-            
+
             element.parentNode.insertBefore(opt.displayElement, element.nextSibling);
-            
+
             opt.displayInput = Ink.s('input', opt.displayElement);
             opt.displayOptions = Ink.s('.pink-auto-complete-options', opt.displayElement);
 
@@ -299,13 +300,19 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
                     updateValueAndLabel(opt.binding, '', undefined, opt.displayInput, element);
                 });
             }
-            
-            // handle the choices being updated in a computed, so the update function doesn't 
+
+            if (opt.focus) {
+                window.setTimeout(function() {
+                    opt.displayInput.focus();
+                }, 250);
+            }
+
+            // handle the choices being updated in a computed, so the update function doesn't
             // have to do it each time the value is updated.
             mappedSource = ko.computed(function () {
                 return buildDataSource(opt.dataSource, opt.labelProp, opt.valueProp);
             }, viewModel);
-            
+
             subscription = mappedSource.subscribe(function (newValue) {
                 var ul = opt.displayOptions.firstChild.firstChild;
 
@@ -315,7 +322,7 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
 
             opt.source = mappedSource();
             opt.change = handleValueChange;
-            
+
             ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
                 subscription.dispose();
                 mappedSource.dispose();
@@ -324,12 +331,12 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
             buildAutoComplete(opt);
         },
 
-        
+
         /*
          * Knockout custom binding update
-         * 
+         *
          * Updates the autocomplete based on a model change
-         * 
+         *
          */
         update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
             var dataSource = valueAccessor();
@@ -343,18 +350,18 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
             var currentModelValue;
             var selectedItem;
             var displayText;
-            
+
             if (modelValue) {
                 currentModelValue = ko.unwrap(modelValue);
-                
+
                 //Set the hidden box to be the same as the viewModels Bound property
                 element.value = currentModelValue;
 
-                // If the value is different from the label's, let's find the corresponding label 
+                // If the value is different from the label's, let's find the corresponding label
                 if (valueProp != labelProp) {
                     //Go through the source and find the id, and use its label to set the autocomplete
                     selectedItem = findSelectedItem(dataSource, binding, currentModelValue);
-                    
+
                     if (selectedItem) {
                         displayText = labelProp ? ko.unwrap(selectedItem[labelProp]) : ko.unwrap(selectedItem).toString();
                         displayInput.value = displayText;
@@ -373,6 +380,6 @@ Ink.createModule('Pink.Data.AutoComplete', '1', ['Pink.Data.Binding_1', 'Ink.Dom
             }
         }
     };
-    
+
     return {};
 });
